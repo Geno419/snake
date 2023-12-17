419fafb8cee747362b3ea556a22bfec415aa275c
Geno419 <ogbeidegild@gmail.com>
2023-12-17 10:51:27 +0000

    updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-site can be accessed at: https://geno419.github.io/snake/
+# Gild's Snake Game
+
+Hey folks! 👋 Welcome to my snake game – a classic arcade game built with JavaScript and HTML canvas. Dive into the code and have fun customizing your own version!
+
+## Getting Started
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/your-username/your-snake-game.git
+   ```
+
+2. Open `index.html` in your web browser.
+
+3. Control the snake using arrow keys (Up, Down, Left, Right) or W, A, S, D keys.
+
+## Code Overview
+
+### Snake Logic
+
+- The snake is controlled by arrow keys or W, A, S, D keys.
+- The game is initialized with a grid-like canvas with a specified number of rows and columns.
+- The snake grows longer each time it consumes the food (apple).
+
+### Images
+
+- Utilizes images for the background, apple, and snake head.
+- Customize images by replacing the existing ones in the `images` folder.
+
+### Styling
+
+- The game's styling is defined in the `style.css` file.
+- Feel free to tweak styles to match your preferences.
+
+### Gameplay
+
+- The game ends if the snake hits the canvas boundaries or collides with itself.
+- Score is displayed, and the game can be restarted after reaching the game over state.
+
+## Customization
+
+- **Images**: Replace images in the `images` folder to give your game a unique look.
+- **Styling**: Modify the styles in `style.css` to personalize the game's appearance.
+
+## Gameplay
+
+- **Control**: Use arrow keys (Up, Down, Left, Right) or W, A, S, D keys to control the snake.
+- **Scoring**: Increase your score by eating apples. The game ends if the snake collides with the canvas boundaries or itself.
+
+## Access the Game
+
+The game is hosted on GitHub Pages and can be accessed [here](https://geno419.github.io/snake/).
+
+## License
+
+Feel free to enjoy and customize this snake game for your own use. No need for contributions – let's keep it personal and fun!
+
+---
+
+Have a blast playing and customizing your snake game! If you have any questions or need assistance, feel free to reach out. Happy coding! 🐍🕹️
--- script.js
+++ script.js
@@ -19,122 +19,114 @@ let gameOver = false;
 let score = 0;
 
 let backgroundImage = new Image();
-backgroundImage.src = 'images/grass.jpg';
+backgroundImage.src = "images/grass.jpg";
 
 let appleImage = new Image();
-appleImage.src = 'images/apple.png';
+appleImage.src = "images/apple.png";
 
 let faceImage = new Image();
-faceImage.src = 'images/face.jpg';
+faceImage.src = "images/face.jpg";
 
 window.onload = function () {
-    board = document.getElementById("board");
-    board.height = total_row * blockSize;
-    board.width = total_col * blockSize;
-    context = board.getContext("2d");
+  board = document.getElementById("board");
+  board.height = total_row * blockSize;
+  board.width = total_col * blockSize;
+  context = board.getContext("2d");
 
-    placeFood();
-    document.addEventListener("keyup", changeDirection);
-    setInterval(update, 120);
-}
+  placeFood();
+  document.addEventListener("keyup", changeDirection);
+  setInterval(update, 120);
+};
 function update() {
-    if (gameOver) {
-        return;
+  if (gameOver) {
+    return;
+  }
+
+  context.drawImage(backgroundImage, 0, 0, board.width, board.height);
+
+  context.drawImage(appleImage, foodX, foodY, blockSize, blockSize);
+
+  context.drawImage(faceImage, snakeX, snakeY, blockSize, blockSize);
+
+  if (snakeX == foodX && snakeY == foodY) {
+    snakeBody.push([foodX, foodY]);
+    placeFood();
+    incrementScore();
+  }
+
+  for (let i = snakeBody.length - 1; i > 0; i--) {
+    snakeBody[i] = snakeBody[i - 1];
+  }
+  if (snakeBody.length) {
+    snakeBody[0] = [snakeX, snakeY];
+  }
+
+  context.fillStyle = "white";
+  for (let i = 0; i < snakeBody.length; i++) {
+    context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+  }
+
+  snakeX += speedX * blockSize;
+  snakeY += speedY * blockSize;
+
+  context.drawImage(faceImage, snakeX, snakeY, blockSize, blockSize);
+
+  if (
+    snakeX < 0 ||
+    snakeX > total_col * blockSize ||
+    snakeY < 0 ||
+    snakeY > total_row * blockSize
+  ) {
+    gameOver = true;
+    endGame();
+  }
+
+  for (let i = 0; i < snakeBody.length; i++) {
+    if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
+      gameOver = true;
+      endGame();
     }
-
-    context.drawImage(backgroundImage, 0, 0, board.width, board.height);
-
-    context.drawImage(appleImage, foodX, foodY, blockSize, blockSize);
-
-    context.drawImage(faceImage, snakeX, snakeY, blockSize, blockSize);
-
-    if (snakeX == foodX && snakeY == foodY) {
-        snakeBody.push([foodX, foodY]);
-        placeFood();
-        incrementScore();
-    }
-
-    for (let i = snakeBody.length - 1; i > 0; i--) {
-        snakeBody[i] = snakeBody[i - 1];
-    }
-    if (snakeBody.length) {
-        snakeBody[0] = [snakeX, snakeY];
-    }
-
-    context.fillStyle = "white";
-    for (let i = 0; i < snakeBody.length; i++) {
-        context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
-    }
-
-    snakeX += speedX * blockSize;
-    snakeY += speedY * blockSize;
-
-    context.drawImage(faceImage, snakeX, snakeY, blockSize, blockSize);
-
-    if (snakeX < 0 || snakeX > total_col * blockSize || snakeY < 0 || snakeY > total_row * blockSize) {
-        gameOver = true;
-        endGame();
-    }
-
-    for (let i = 0; i < snakeBody.length; i++) {
-        if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
-            gameOver = true;
-            endGame();
-        }
-    }
+  }
 }
 function changeDirection(e) {
-    if (
-        (e.code == "ArrowUp" || e.key === "w") &&
-        speedY != 1
-    ) {
-        speedX = 0;
-        speedY = -1;
-    } else if (
-        (e.code == "ArrowDown" || e.key === "s") &&
-        speedY != -1
-    ) {
-        speedX = 0;
-        speedY = 1;
-    } else if (
-        (e.code == "ArrowLeft" || e.key === "a") &&
-        speedX != 1
-    ) {
-        speedX = -1;
-        speedY = 0;
-    } else if (
-        (e.code == "ArrowRight" || e.key === "d") &&
-        speedX != -1
-    ) {
-        speedX = 1;
-        speedY = 0;
-    }
+  if ((e.code == "ArrowUp" || e.key === "w") && speedY != 1) {
+    speedX = 0;
+    speedY = -1;
+  } else if ((e.code == "ArrowDown" || e.key === "s") && speedY != -1) {
+    speedX = 0;
+    speedY = 1;
+  } else if ((e.code == "ArrowLeft" || e.key === "a") && speedX != 1) {
+    speedX = -1;
+    speedY = 0;
+  } else if ((e.code == "ArrowRight" || e.key === "d") && speedX != -1) {
+    speedX = 1;
+    speedY = 0;
+  }
 }
 
-
 function placeFood() {
-    foodX = Math.floor(Math.random() * total_col) * blockSize;
-    foodY = Math.floor(Math.random() * total_row) * blockSize;
+  foodX = Math.floor(Math.random() * total_col) * blockSize;
+  foodY = Math.floor(Math.random() * total_row) * blockSize;
 }
 
 function incrementScore() {
-    score++;
-    document.getElementById("score").innerText = "Score: " + score;
+  score++;
+  document.getElementById("score").innerText = "Score: " + score;
 }
 
 function endGame() {
-    alert('Game Over! Score: ' + score + '\nPress OK to restart.');
-    restartGame();
+  alert("Game Over! Score: " + score + "\nPress OK to restart.");
+  restartGame();
 }
 
 function restartGame() {
-    snakeX = blockSize * 5;
-    snakeY = blockSize * 5;
-    speedX = 0;
-    speedY = 0;
-    snakeBody = [];
-    placeFood();
-    gameOver = false;
-    score = 0;
-    document.getElementById("score").innerText = "Score: 0";
+  snakeX = blockSize * 5;
+  snakeY = blockSize * 5;
+  speedX = 0;
+  speedY = 0;
+  snakeBody = [];
+  placeFood();
+  gameOver = false;
+  score = 0;
+  document.getElementById("score").innerText = "Score: 0";
 }
